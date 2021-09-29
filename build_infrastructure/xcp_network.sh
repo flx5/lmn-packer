7907e001feb349a86f761091754259f3611cf900
flx5 <1330854+flx5@users.noreply.github.com>
2021-09-30 00:28:25 +0200

    Debug xcp network

--- build_infrastructure/xcp_network.sh
+++ build_infrastructure/xcp_network.sh
@@ -9,9 +9,13 @@ wget -O/dev/null --retry-connrefused --wait=10 --tries=20 --no-check-certificate
 # eth1 green
 
 RED_NETWORK=$(xe network-list bridge=xenbr0 --minimal)
+echo "Red UUID: $RED_NETWORK"
+
 xe network-param-set name-label=Red uuid=$RED_NETWORK
 
 GREEN_NETWORK=$(xe network-list bridge=xenbr1 --minimal)
+echo "Green UUID: $GREEN_NETWORK"
+
 xe network-param-set name-label=Green uuid=$GREEN_NETWORK
 
 GREEN_PIF=$(xe pif-list device=eth1 --minimal)
